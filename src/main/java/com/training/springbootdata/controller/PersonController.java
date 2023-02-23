83316198b89fcc8169189907bbfffd4c9067e15a
Cesar Vasquez <cesarvf90@gmail.com>
2023-02-23 17:39:22 +0100

    Add SLF4J and Lombok
    Add SLF4J and Lombok for logging purposes and spicing up Java code.

--- src/main/java/com/training/springbootdata/controller/PersonController.java
+++ src/main/java/com/training/springbootdata/controller/PersonController.java
@@ -1,11 +1,15 @@
 package com.training.springbootdata.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.training.springbootdata.model.Person;
 import com.training.springbootdata.repository.PersonRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/persons")
 public class PersonController {
@@ -19,6 +23,17 @@ public class PersonController {
     @GetMapping("")
     public List<Person> getPersons() {
         return personRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> getElementById(@PathVariable Long id) {
+        Optional<Person> elementOptional = personRepository.findById(id);
+        log.info("Element found by id: " + elementOptional);
+        if (elementOptional.isPresent()) {
+            return ResponseEntity.ok(elementOptional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping("")
